5fe16b687e0cb9ff0d528144b42f618fb33516c6
briscagit <briscagit@gmail.com>
2018-07-23 23:10:58 -0400

    add .js file

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var image = document.querySelector('img');
+
+image.onclick = function() {
+    //alert("clicked image");
+    //var myProperty = image.style.filter;
+
+    //var f = image.style.filter
+    // filter is off or image has not been clicked
+    if (image.style.filter === 'invert(0%)' || image.style.filter === '') image.style.filter = 'invert(100%)';
+    // filter is on
+    else image.style.filter = 'invert(0%)';
+}
